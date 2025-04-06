6f58775a2509e9fd76346099b01a6e50656a6668
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2025-04-06 14:42:23 -0400

    Now can safely write text to the console.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
+
 use core::panic::PanicInfo;
+use core::fmt::Write;
 
 
 #[panic_handler]
@@ -13,13 +17,8 @@ static HELLO: &[u8] = b"Hello, World";
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    vga_buffer::WRITER.lock().write_str("Hello Again").unwrap();
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {}, {}", 42, 1.337).unwrap();
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
